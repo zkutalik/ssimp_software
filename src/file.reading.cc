87fed57d0adc744138c89b08057b69164047c018
Aaron McDaid <aaronmcdaid@gmail.com>
2017-03-28 16:52:35 +0000

    Skip the '##' and print the header

--- src/file.reading.cc
+++ src/file.reading.cc
@@ -1,9 +1,13 @@
 #include "file.reading.hh"
 
 #include <iostream>
+#include <fstream>
 
 #include "other/DIE.hh"
 #include "other/PP.hh"
+
+using std:: ifstream;
+using std:: string;
 
 #include "fwd/src/file.reading.hh"
 
@@ -19,10 +23,24 @@ GenotypeFileHandle      read_in_a_raw_ref_file(std:: string file_name) {
 }
 
 FWD(file_reading)
+static
 GenotypeFileHandle      read_in_a_raw_ref_file_as_VCF(std:: string file_name) {
     PP(file_name);
+    ifstream f(file_name);
+    string line;
+    while(getline(f, line)) {
+        if(line.at(0) == '#' && line.at(1) == '#')
+            continue; // skip these ## lines
+        parse_header(line);
+        break;
+    }
     return {};
 }
 
+FWD(file_reading)
+static
+void   parse_header( string      const & header_line ) {
+    PP(header_line);
+}
 
 } // namespace file_reading
