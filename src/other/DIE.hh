bdb24d9279fbcb461416c8e1d0910e1ba9c49d3e
Aaron McDaid <aaronmcdaid@gmail.com>
2017-03-28 18:26:01 +0000

    DIE.hh: WARNING

--- src/other/DIE.hh
+++ src/other/DIE.hh
@@ -4,3 +4,5 @@
 #define error_print_and_exit(...) ([&]()->bool{ std::cerr << '\n' << "Exiting: "  __FILE__  ":" << __LINE__ << "\tError: " << __VA_ARGS__; std::cerr << '\n'; exit(1);}())
 #endif
 #define DIE(...) error_print_and_exit(__VA_ARGS__)
+
+#define WARNING(...) ([&]()->bool{ std::cerr << '\n' << "WARNING: " << __VA_ARGS__; std::cerr << '\n'; return false; }())
