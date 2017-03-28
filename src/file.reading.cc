8e040347eb83a86e2e5b5b3770d196b5c676c6d8
Aaron McDaid <aaronmcdaid@gmail.com>
2017-03-28 16:04:42 +0000

    Aaron's crazy FWD trick

--- src/file.reading.cc
+++ src/file.reading.cc
@@ -5,14 +5,12 @@
 #include "other/DIE.hh"
 #include "other/PP.hh"
 
+#include "fwd/file.reading.hh"
+
 namespace file_reading {
 
 struct PlainVCFfile : public Genotypes_I {
 };
-
-GenotypeFileHandle      read_in_a_raw_ref_file_as_VCF(std:: string file_name) {
-    return {};
-}
 
 GenotypeFileHandle      read_in_a_raw_ref_file(std:: string file_name) {
     PP(file_name);
@@ -21,5 +19,12 @@ GenotypeFileHandle      read_in_a_raw_ref_file(std:: string file_name) {
     // But for now, we'll just assume a plain (non-gzipped) vcf file.
     return read_in_a_raw_ref_file_as_VCF(file_name);
 }
+
+FWD
+file_reading::
+GenotypeFileHandle      read_in_a_raw_ref_file_as_VCF(std:: string file_name) {
+    return {};
+}
+
 
 } // namespace file_reading
