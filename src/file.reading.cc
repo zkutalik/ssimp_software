221533f1d4920115adca751e94655838cfc1fcb1
Aaron McDaid <aaronmcdaid@gmail.com>
2017-03-28 16:13:38 +0000

    make a 'src' subdir inside 'fwd'. Note, this doesn't compile

--- src/file.reading.cc
+++ src/file.reading.cc
@@ -5,7 +5,9 @@
 #include "other/DIE.hh"
 #include "other/PP.hh"
 
-#include "fwd/file.reading.hh"
+namespace file_reading {
+#include "fwd/src/file.reading.hh"
+}
 
 namespace file_reading {
 
@@ -13,8 +15,6 @@ struct PlainVCFfile : public Genotypes_I {
 };
 
 GenotypeFileHandle      read_in_a_raw_ref_file(std:: string file_name) {
-    PP(file_name);
-
     // I really should detect the file-type.
     // But for now, we'll just assume a plain (non-gzipped) vcf file.
     return read_in_a_raw_ref_file_as_VCF(file_name);
