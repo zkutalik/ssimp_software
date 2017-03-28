2321d7ab68166f71293d116ec2ed55c51315cc2c
Aaron McDaid <aaronmcdaid@gmail.com>
2017-03-28 19:50:54 +0000

    utils:: lexical_cast<int>

--- src/other/utils.hh
+++ src/other/utils.hh
@@ -21,4 +21,20 @@ std::ostream & operator<< (std:: ostream &o, const std:: vector<T> &v) {
     return o;
 }
 
+template<typename T>
+int lexical_cast(std:: string const & s);
+
+template<>
+inline
+int lexical_cast<int>(std:: string const & s) {
+    int d;
+    int n;
+    int ret = sscanf(s.c_str(), "%d %n", &d, &n);
+    if(ret == 1 && n == ssize(s)) {
+        return d;
+    }
+    else
+        throw std:: invalid_argument{"can't parse this int"};
+}
+
 } // namespace utils
