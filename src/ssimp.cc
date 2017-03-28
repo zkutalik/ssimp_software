d88e5a8311e76f8cfb060cf65ad556c81dc1418f
Aaron McDaid <aaronmcdaid@gmail.com>
2017-03-28 15:14:47 +0000

    bring in 'other/PP.hh

--- src/ssimp.cc
+++ src/ssimp.cc
@@ -3,6 +3,7 @@
 #include "options.hh"
 
 #include "other/DIE.hh"
+#include "other/PP.hh"
 
 using std:: cout;
 using std:: endl;
@@ -10,4 +11,6 @@ using std:: endl;
 int main(int argc, char **argv) {
     // all options now read. Start checking they are all present
     options:: read_in_all_command_line_options(argc, argv);
+
+    PP(options:: opt_raw_ref);
 }
