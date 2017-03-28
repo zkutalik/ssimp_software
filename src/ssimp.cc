78401ea313e334c88c59d6f6ac0630826bca84f8
Aaron McDaid <aaronmcdaid@gmail.com>
2017-03-28 14:40:53 +0000

    start using getopt_long

--- src/ssimp.cc
+++ src/ssimp.cc
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <getopt.h>
+#include "other/DIE.hh"
 
 using std:: cout;
+using std:: endl;
 
-int main() {
-    cout << "ssimp v0.0.0-2017-03-28\n";
+int main(int argc, char **argv) {
+    while(1) { // while there are still more options to be processed
+        int long_option_index;
+        static struct option long_options[] = {
+            //{"header.in",  no_argument, &opt_headerin,  1 }, // a boolean flag
+            {"raw.ref",  required_argument, 0,  2 }, // last arg on each line must be greater than 1
+            {0,         0,                 0,  0 } // must have this line of zeroes at the end
+        };
+        int c = getopt_long(argc, argv, "-", long_options, &long_option_index);
+        if (c == -1)
+            break;
+        if (c == 1) { // non-option
+            DIE("There shouldn't be any non-options args to this program: ["<<optarg<<"]");
+        }
+        if (c == 2) {
+            //assert(string("z_compare") == long_options[long_option_index].name);
+            //zs_to_compare.emplace_back( optarg, read_in_a_vector(optarg) );
+        }
+    }
+    // all options now read. Start checking they are all present
 }
