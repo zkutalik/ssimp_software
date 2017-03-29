7bd222df39a3aab0504e4c0b7142a44b665738e5
Aaron McDaid <aaronmcdaid@gmail.com>
2017-03-29 12:24:53 +0000

    FWD in ssimp

--- src/ssimp.cc
+++ src/ssimp.cc
@@ -9,6 +9,8 @@
 using std:: cout;
 using std:: endl;
 
+#include "fwd/src/ssimp.hh"
+
 int main(int argc, char **argv) {
     // all options now read. Start checking they are all present
     options:: read_in_all_command_line_options(argc, argv);
