c7352114b53742df2e8cfb7d97eb36e790b5e122
Aaron McDaid <aaronmcdaid@gmail.com>
2017-03-29 12:25:23 +0000

    Testing that SNPIterator a little

--- src/ssimp.cc
+++ src/ssimp.cc
@@ -19,5 +19,26 @@ int main(int argc, char **argv) {
         PP(options:: opt_raw_ref);
         auto raw_ref_file = file_reading:: read_in_a_raw_ref_file(options:: opt_raw_ref);
         PP(raw_ref_file->number_of_snps());
+
+        ssimp:: quickly_list_the_regions(raw_ref_file);
     }
 }
+
+namespace ssimp{
+
+FWD(ssimp)
+void quickly_list_the_regions(file_reading:: GenotypeFileHandle raw_ref_file) {
+    int total_number_of_SNPs = raw_ref_file->number_of_snps();
+    PP (total_number_of_SNPs);
+
+    auto b = file_reading:: SNPiterator:: begin_from_file(raw_ref_file);
+    auto e = file_reading:: SNPiterator::   end_from_file(raw_ref_file);
+
+    while(b <  e) {
+        PP(b.get_chrpos());
+        ++b;
+    }
+    PP("end");
+}
+
+} // namespace ssimp
