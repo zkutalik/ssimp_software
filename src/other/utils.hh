888b7791161aee9c55b499a45c8b7d90d9c138a7
Aaron McDaid <aaronmcdaid@gmail.com>
2017-04-19 15:50:21 +0200

    utils:: apply, copied back from c++17

--- src/other/utils.hh
+++ src/other/utils.hh
@@ -147,4 +147,24 @@ struct and_all_impl<T0, T1, Ts...> {
     static constexpr
     auto  impl(T0 t0, T1 t1, Ts ...ts) { return and_all(t0 && t1, ts...); }
 };
+
+// This is my own 'apply', as it's officially only in c++17, not c++14
+namespace detail {
+template <class F, class Tuple, std::size_t... I>
+constexpr decltype(auto) apply_impl(F &&f, Tuple &&t, std::index_sequence<I...>)
+{
+    return std::forward<F>(f)(std::get<I>(std::forward<Tuple>(t))...);
+}
+}  // namespace detail
+
+template <class F, class Tuple>
+constexpr decltype(auto) apply(F &&f, Tuple &&t)
+{
+    return detail::apply_impl(
+        std::forward<F>(f), std::forward<Tuple>(t),
+        std::make_index_sequence<std::tuple_size<std::decay_t<Tuple>>{}>{});
+}
+
+
+
 } // namespace utils
