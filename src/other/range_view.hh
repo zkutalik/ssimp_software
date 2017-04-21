a8bf3bd9dc5305bb82c724a78a347f69c65c1e09
Aaron McDaid <aaronmcdaid@gmail.com>
2017-04-21 18:32:33 +0200

    range_view.hh: unzip_map

--- src/other/range_view.hh
+++ src/other/range_view.hh
@@ -7,9 +7,11 @@ namespace view {
     struct {} unzip_foreach;
     struct {} collect;
     struct {} unzip_collect_transpose;
+    struct {} unzip_map;
 
     template<typename R, typename tag>
     struct temporary_tagged_holder {
+        static_assert(!std:: is_reference<R>{}, "");
         R m_r;
         temporary_tagged_holder(R r) : m_r(std::move(r)) {}
     };
@@ -34,6 +36,10 @@ namespace view {
     template<typename R>
     auto operator| (R r, decltype(unzip_foreach) ) {
         return temporary_tagged_holder<R, decltype(unzip_foreach)> { std::move(r) };
+    }
+    template<typename R>
+    auto operator| (R r, decltype(unzip_map) ) {
+        return temporary_tagged_holder<R, decltype(unzip_map)> { std::move(r) };
     }
     template<typename R>
     auto operator| (R r, decltype(foreach) ) {
@@ -72,6 +78,25 @@ namespace view {
                             , range:: pull(r_holder.m_r)
             );
         }
+    }
+    template<typename R, typename F>
+    auto operator| (temporary_tagged_holder<R, decltype(unzip_map)> r_holder, F f)
+    {
+        struct unzip_map_t {
+            R m_r;
+            F f;
+            bool empty() const {
+                return m_r.empty();
+            }
+            auto front_val() {
+                return
+                utils:: apply   ( std::forward<F>(f)
+                                , range:: front_val(m_r)
+                                );
+            }
+            void advance() { m_r.advance(); }
+        };
+        return unzip_map_t{ move(r_holder.m_r), std:: move(f) };
     }
     template<typename R, typename F>
     void operator| (temporary_tagged_holder<R, decltype(foreach)> r_holder, F && f)
