aad5a00ef442fa376cb17552d524825b76d95420
Aaron McDaid <aaronmcdaid@gmail.com>
2017-04-30 13:19:37 +0200

    |view::unzip_filter|

--- src/other/range_view.hh
+++ src/other/range_view.hh
@@ -15,6 +15,7 @@ namespace view {
 
     struct {} ref_wraps;
     struct {} map;
+    struct {} unzip_filter;
     struct {} unzip_map;
 
     template<typename R>
@@ -35,12 +36,51 @@ namespace view {
     }
 
     template<typename R>
+    auto operator| (R r, decltype(unzip_filter) ) {
+        return detail:: temporary_tagged_holder<R, decltype(unzip_filter)> { std::move(r) };
+    }
+    template<typename R>
     auto operator| (R r, decltype(unzip_map) ) {
         return detail:: temporary_tagged_holder<R, decltype(unzip_map)> { std::move(r) };
     }
     template<typename R>
     auto operator| (R r, decltype(map) ) {
         return detail:: temporary_tagged_holder<R, decltype(map)> { std::move(r) };
+    }
+
+    template<typename R, typename F>
+    struct unzip_filter_t {
+        R m_r;
+        F m_f;
+
+        static_assert(!std::is_reference<R>{} ,"");
+        static_assert(!std::is_reference<F>{} ,"");
+
+        unzip_filter_t(R r, F f) : m_r(std::move(r)), m_f(std::move(f))
+        {
+            skip_filter_mismatches();
+        }
+        bool empty() const {
+            return m_r.empty();
+        }
+        auto front_val() -> AMD_RANGE_DECLTYPE_AND_RETURN( range:: front_val(m_r) )
+        void advance() {
+            m_r.advance();
+            skip_filter_mismatches();
+        }
+        void skip_filter_mismatches() {
+            while   (   !empty()
+                     && !utils:: apply  ( std::forward<F>(m_f)
+                                        , range:: front_val(m_r)
+                                        )) {
+                m_r.advance();
+            }
+        }
+    };
+    template<typename R, typename F>
+    auto operator| (detail:: temporary_tagged_holder<R, decltype(unzip_filter)> r_holder, F f)
+    {
+        return unzip_filter_t<R,F>{ move(r_holder.m_r), std:: move(f) };
     }
 
     template<typename R, typename F>
