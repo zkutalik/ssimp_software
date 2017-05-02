31a960e1953f1dae9f903967c3ea4124bc93272e
Aaron McDaid <aaronmcdaid@gmail.com>
2017-05-03 01:11:16 +0200

    Doh. I had forgotten to actually commit zlib-vector.{cc,hh}

---
+++ zlib-vector.hh
@@ -0,0 +1,5 @@
+#include<vector>
+
+std:: vector<unsigned char> zlib_deflate_vector(std:: vector<unsigned char> src);
+std:: vector<unsigned char> zlib_inflate_vector(std:: vector<unsigned char> src);
+
