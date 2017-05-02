671a71638e847efe8ef575dd478163656747e700
Aaron McDaid <aaronmcdaid@gmail.com>
2017-05-03 00:45:48 +0200

    Clean up inf() a little, remove the outer loop

--- zpipe.cc
+++ zpipe.cc
@@ -133,41 +133,33 @@ vector<unsigned char> inf(vector<unsigned char> src)
     if (ret != Z_OK)
         throw exc:: error_from_inflateInit_t{ret};
 
-    /* decompress until deflate stream ends or end of file */
+    strm.avail_in = src.size();
+    strm.next_in = src.data();
+
+    /* run inflate() on input until output buffer not full */
     do {
-        strm.avail_in = src.size();
-        if (strm.avail_in == 0)
-            break;
-        strm.next_in = src.data();
+        strm.avail_out = CHUNK;
+        strm.next_out = out;
+        ret = inflate(&strm, Z_NO_FLUSH);
+        assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+        switch (ret) {
+        case Z_NEED_DICT:
+            ret = Z_DATA_ERROR;     /* and fall through */
+        case Z_DATA_ERROR:
+        case Z_MEM_ERROR:
+            (void)inflateEnd(&strm);
+            throw exc:: error_from_inflateInit_t{ret};
+        }
+        have = CHUNK - strm.avail_out;
+        result.insert(result.end(), out, out+have);
+    } while (strm.avail_out == 0);
 
-        /* run inflate() on input until output buffer not full */
-        do {
-            strm.avail_out = CHUNK;
-            strm.next_out = out;
-            ret = inflate(&strm, Z_NO_FLUSH);
-            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
-            switch (ret) {
-            case Z_NEED_DICT:
-                ret = Z_DATA_ERROR;     /* and fall through */
-            case Z_DATA_ERROR:
-            case Z_MEM_ERROR:
-                (void)inflateEnd(&strm);
-                throw exc:: error_from_inflateInit_t{ret};
-            }
-            have = CHUNK - strm.avail_out;
-            result.insert(result.end(), out, out+have);
-        } while (strm.avail_out == 0);
-
-        assert(ret == Z_STREAM_END);
-
-        /* done when inflate() says it's done */
-    } while (ret != Z_STREAM_END);
+    // We deflated one big chunk of input, so ret
+    // will be Z_STREAM_END now
+    assert(ret == Z_STREAM_END);
 
     /* clean up and return */
     (void)inflateEnd(&strm);
-    if(ret != Z_STREAM_END) {
-        throw exc:: error_from_inflateInit_t{Z_DATA_ERROR};
-    }
     return result;
 }
 
