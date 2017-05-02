f3a51b9195d2b86d694af500b6c8f250cc07e311
Aaron McDaid <aaronmcdaid@gmail.com>
2017-05-03 00:39:41 +0200

    zerr() no longer used there

--- zpipe.cc
+++ zpipe.cc
@@ -172,31 +172,6 @@ vector<unsigned char> inf(vector<unsigned char> src)
     return result;
 }
 
-/* report a zlib or i/o error */
-void zerr(int ret)
-{
-    fputs("zpipe: ", stderr);
-    switch (ret) {
-    case Z_ERRNO:
-        if (ferror(stdin))
-            fputs("error reading stdin\n", stderr);
-        if (ferror(stdout))
-            fputs("error writing stdout\n", stderr);
-        break;
-    case Z_STREAM_ERROR:
-        fputs("invalid compression level\n", stderr);
-        break;
-    case Z_DATA_ERROR:
-        fputs("invalid or incomplete deflate data\n", stderr);
-        break;
-    case Z_MEM_ERROR:
-        fputs("out of memory\n", stderr);
-        break;
-    case Z_VERSION_ERROR:
-        fputs("zlib version mismatch!\n", stderr);
-    }
-}
-
 /* compress or decompress from stdin to stdout */
 int main(int argc, char **argv)
 {
