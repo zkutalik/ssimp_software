fe29266aa363cd4a3b23cde68c1e6c4e2db9a6d9
Aaron McDaid <aaronmcdaid@gmail.com>
2017-05-03 12:41:28 +0200

    range_from.hh: don't need those ADL-helping lines after all, now that I declared the range:: ones earlier.

--- range_from.hh
+++ range_from.hh
@@ -32,11 +32,6 @@ auto    ifstream(T_vl && f) {
     return from_ifstream_t<T_vl>( AMD_FORWARD(f) );
 }
 
-/* Next few lines are needed to help ADL - they forward *as-is* */
-template<typename T> auto  begin        (T&&t)  -> AMD_RANGE_DECLTYPE_AND_RETURN( range:: begin     (AMD_FORWARD(t)) )
-template<typename T> auto  end          (T&&t)  -> AMD_RANGE_DECLTYPE_AND_RETURN( range:: end       (AMD_FORWARD(t)) )
-template<typename T> auto  front_val    (T&&t)  -> AMD_RANGE_DECLTYPE_AND_RETURN( range:: front_val (AMD_FORWARD(t)) )
-
 } //namespace from
 } //namespace range
 
