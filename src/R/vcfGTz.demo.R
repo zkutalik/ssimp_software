53dd5c00415f53b6d5d891d7b07605743f58349f
Aaron McDaid <aaronmcdaid@gmail.com>
2017-05-05 00:50:58 +0200

    R: vcfGTz_get_internal_offsetsnum_SNPs(). Returns NumericVector instead of IntegerVector

--- src/R/vcfGTz.demo.R
+++ src/R/vcfGTz.demo.R
@@ -5,3 +5,6 @@ filename='ref/compressed/50mb.vcfGTz'
 num.SNPs = vcfGTz_num_SNPs(filename)
 PP(num.SNPs)
 
+file_offsets = vcfGTz_get_internal_offsets(filename, 0:3 %c% -1:3)
+PP(file_offsets)
+
--- src/R/vcfGTz.reader.R
+++ src/R/vcfGTz.reader.R
@@ -7,7 +7,7 @@ delayedAssign("vcfGTz_num_SNPs", Rcpp:: cppFunction( plugins='cpp14', includes=c
 			// read in the "magic" string
 			auto magic = reader.read_string0();
 			if(magic != "vcfGTz.0.0.2")
-				return {-1};
+				stop("wrong file type?");
 
 			// skip over the first block
 			auto offset_over_this_block = reader.read_offset_at_start_of_block();
@@ -17,9 +17,53 @@ delayedAssign("vcfGTz_num_SNPs", Rcpp:: cppFunction( plugins='cpp14', includes=c
 			auto another_offset = reader.read_offset_at_start_of_block();
 			auto description_of_second_block = reader.read_string0();
 			if(description_of_second_block != "offsets.into.previous.block")
-				return {-1};
+				stop("wrong file type?");
 
 			int num_lines = reader.read_uint64_t();
 			int num_SNPs = num_lines - 1; // as the first one is the header
 			return {num_SNPs};
+}'))
+delayedAssign("vcfGTz_get_internal_offsets", Rcpp:: cppFunction( plugins='cpp14', includes=c("#include<fstream>" ,'#include "/home/aaron/Research/Code/Imputation/ssimp_software/src/vcfGTz_reader.hh"')
+	, 'NumericVector vcfGTz_get_internal_offsetsnum_SNPs(CharacterVector filename, IntegerVector indices) {
+			std:: string filename_ = {filename[0]};
+			vcfGTz:: vcfGTz_reader reader(filename_);
+
+			// read in the "magic" string
+			auto magic = reader.read_string0();
+			if(magic != "vcfGTz.0.0.2")
+				stop("wrong file type?");
+
+			// skip over the first block
+			auto offset_over_this_block = reader.read_offset_at_start_of_block();
+			reader.seek_relative(offset_over_this_block-8);
+
+			// now read the start of the second block
+			auto another_offset = reader.read_offset_at_start_of_block();
+			auto description_of_second_block = reader.read_string0();
+			if(description_of_second_block != "offsets.into.previous.block")
+				stop("wrong file type?");
+
+			int num_lines = reader.read_uint64_t();
+			int num_SNPs = num_lines - 1; // as the first one is the header
+
+			auto remember_this_position = reader.m_f.tellg();
+
+			NumericVector results; // Numeric is like a 52-bit integer, in case the vcfGTz files are really big
+
+			for( auto && i : indices ) {
+				if(i>=1 && i<=num_SNPs) {
+					reader.m_f.seekg( remember_this_position .operator+( 8*i ));
+					// 0 would be the header, but we will use base-1
+					// as this is R
+					auto u64 = reader.read_uint64_t();
+					results.push_back(u64);
+
+					auto verify = results.at(results.size()-1);
+					u64 == verify || (stop("problem with offsets? Really big file?"),false);
+				}
+				else
+					results.push_back(NA_REAL);
+			}
+
+			return results;
 }'))
