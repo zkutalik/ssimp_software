16b568b5184ef5617af2926cb5460fbe7c424254
Aaron McDaid <aaronmcdaid@gmail.com>
2017-05-05 18:38:37 +0200

    R: demo: gather that into a nice data.table

--- src/R/vcfGTz.demo.R
+++ src/R/vcfGTz.demo.R
@@ -7,12 +7,13 @@ num.SNPs = vcfGTz_num_SNPs(filename)
 PP(num.SNPs)
 
 file_offsets = vcfGTz_get_internal_offsets(filename, 0:3 %c% -1:3 %c% num.SNPs %c% (num.SNPs+1))
-PP(file_offsets)
+PP(length(file_offsets))
 
-for(which.field in 1:7) {
-    x = vcfGTz_get_1field_from_internal_offsets(filename, file_offsets, which.field)
-    PP(x)
-}
+df = data.frame(lapply(1:7, function(which.field) {
+    vcfGTz_get_1field_from_internal_offsets(filename, file_offsets, which.field)
+}))
+colnames(df) <- ''
+pp(data.table(df))
 
 m = vcfGTz_get_012calls_from_internal_offsets(filename, file_offsets)
 pp(m[                               ,1:15])
