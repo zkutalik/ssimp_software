1a6b5dadfaf248a0f7bc54c4a0a652412b7b205e
Aaron McDaid <aaronmcdaid@gmail.com>
2017-05-05 12:04:36 +0200

    R/ASSERT_for_R.hh, now with __FILE__ and __LINE__

--- src/R/ASSERT_for_R.hh
+++ src/R/ASSERT_for_R.hh
@@ -1,5 +1,7 @@
 #ifdef __GNUC__
-	#define assert(condition) do { if(condition) {} else { stop(std::string("assert() failure: (" #condition ") in ") + __PRETTY_FUNCTION__); } }while(0)
+	#define STRINGIFY_FOR_LINE_NUMBERS_(x) #x
+	#define STRINGIFY_FOR_LINE_NUMBERS(x) STRINGIFY_FOR_LINE_NUMBERS_(x)
+	#define assert(condition) do { if(condition) {} else { stop(std::string("assert() failure: (" #condition ") at [" __FILE__ ":" STRINGIFY_FOR_LINE_NUMBERS(__LINE__) "] in ") + __PRETTY_FUNCTION__); } }while(0)
 #else
 	#define assert(condition) do { if(condition) {} else { stop(            "assert() failure: (" #condition ")"                           ); } }while(0)
 #endif
