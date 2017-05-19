c6f231ff0bfff7638e99587e080a13f107fad8cd
Aaron McDaid <aaronmcdaid@gmail.com>
2017-05-19 20:27:38 +0200

    options.o: --impute.{range,snps}, not implemented yet though

--- src/options.hh
+++ src/options.hh
@@ -8,6 +8,8 @@ extern  std:: string            opt_out;
 extern  int                     opt_window_width;
 extern  int                     opt_flanking_width;
 extern  double                  opt_lambda;
+extern  std:: string            opt_impute_range;
+extern  std:: string            opt_impute_snps;
 
         void                    read_in_all_command_line_options(int argc, char **argv);
 
