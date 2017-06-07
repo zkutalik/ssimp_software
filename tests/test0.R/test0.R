f432c0af5cd927615220171607f2738ff9be87ca
Aaron McDaid <aaronmcdaid@gmail.com>
2017-06-07 17:16:23 +0200

    Tweak tests/test0.R so that it runs under Rscript in a simple way

--- tests/test0.R/test0.R
+++ tests/test0.R/test0.R
@@ -3,8 +3,6 @@ rm(list = ls())
 
 ## set paths
 ## -------------------
-path.out <- "/Users/admin/Documents/Work/Projects/tagging/ssimp_software/tests/test0.R/"
-setwd(path.out)
 
 ## load function
 ## -------------------
@@ -14,9 +12,10 @@ source("../r-function.R")
 
 ## run ssimp function
 ## ---------------
-ssimp(path.gwas = paste0("/Users/admin/Documents/Work/Projects/tagging/ssimp_software/gwas/", "small.UKBB.both.csv"),
-      path.ref = paste0("/Users/admin/Documents/Work/Projects/tagging/ssimp_software/ref/","small.vcf.sample.vcf"), 
-      lambda = "sqrt", 
+ssimp(path.gwas = paste0("../../gwas/", "small.UKBB.both.csv"),
+      path.ref = paste0("../../ref/","small.vcf.sample.vcf"), 
+      path.outdir = './',
+      lambda = 0.01,
       what.to.impute = "Z", 
       gwas.names = c("rnpid", NA, NA, "a2","a1",NA, NA, "z.from.peff", "N"),
       tag.snps = c("rs138257042", "rs200923174", "rs62224618", "rs79847867"),
@@ -28,9 +27,9 @@ ssimp(path.gwas = paste0("/Users/admin/Documents/Work/Projects/tagging/ssimp_sof
 
 ## compare with aarons results
 ## -----------------------------
-out.aaron <- read_tsv("/Users/admin/Documents/Work/Projects/tagging/ssimp_software/tests/test0/imputations.txt", skip = 1, n_max = 3, col_names = FALSE)
+out.aaron <- read_tsv("../test0/imputations.txt", skip = 1, n_max = 3, col_names = FALSE)
 out.aaron
 
-out<- read_tsv("/Users/admin/Documents/Work/Projects/tagging/ssimp_software/tests/test0.R/imputations.txt")
-out
+out.sina.R <- read_tsv("imputations.txt")
+out.sina.R
 
