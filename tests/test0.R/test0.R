4fbcbe3563b807fe866cebbde3fb090358860d0d
Sina Rueeger <sina.rueeger@gmail.com>
2017-06-13 17:13:35 +0200

    fixed bug witgh signs (a1 is ref allele , not alt allele

--- tests/test0.R/test0.R
+++ tests/test0.R/test0.R
@@ -12,12 +12,13 @@ source("../r-function.R")
 
 ## run ssimp function
 ## ---------------
+## a1 is reference allele and a2 alternative allele
 ssimp(path.gwas = paste0("../../gwas/", "small.UKBB.both.csv"),
       path.ref = paste0("../../ref/","small.vcf.sample.vcf"), 
       path.outdir = './',
       lambda = "sqrt",
       what.to.impute = "Z", 
-      gwas.names = c("rnpid", NA, NA, "a2","a1",NA, NA, "z.from.peff", "N"),
+      gwas.names = c("rnpid", NA, NA, "a1","a2",NA, NA, "z.from.peff", "N"),
       tag.snps = c(
                    #, "rs200923174" # just used by SR
                      "rs62224618"
@@ -25,7 +26,7 @@ ssimp(path.gwas = paste0("../../gwas/", "small.UKBB.both.csv"),
                    #, "rs79847867" # why wasn't I used this?
                    ),
       target.snps = c("rs587697622", "rs587755077","rs587654921"),
-      impute.tags = FALSE
+      impute.tags = TRUE
 )
 
 
@@ -38,3 +39,7 @@ out.sina.R <- read_tsv("imputations.txt")
 out.aaron
 out.sina.R
 
+## compare imputation of tags
+## -----------------------------
+out <- read_tsv("imputations_tag.txt")
+out
