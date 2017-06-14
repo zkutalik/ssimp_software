d4d271ae122ba706b25531f6123a67c98921e51e
Aaron McDaid <aaronmcdaid@gmail.com>
2017-06-14 22:01:24 +0000

    test0.R now uses random-created data

--- tests/test0.R/test0.R
+++ tests/test0.R/test0.R
@@ -13,12 +13,12 @@ source("tests/r-function.R")
 ## run ssimp function
 ## ---------------
 ## a1 is reference allele and a2 alternative allele
-ssimp(path.gwas = paste0("gwas/", "small.UKBB.both.csv"),
+ssimp(path.gwas = paste0("gwas/", "small.random.csv"),
       path.ref = paste0("ref/","small.vcf.sample.vcf"), 
       path.outdir = paste0(Sys.getenv('COMMANDDIR'),'/'),
       lambda = "sqrt",
       what.to.impute = "Z", 
-      gwas.names = c("rnpid", NA, NA, "a1","a2",NA, NA, "z.from.peff", "N",'p'),
+      gwas.names = c("rnpid", NA, NA, "a1","a2",NA, NA, "z", NA,NA),
       tag.snps = c(
                    #, "rs200923174" # just used by SR
                      "rs62224618"
