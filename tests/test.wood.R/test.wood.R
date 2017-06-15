5c600ea1bbecdc4fecc3e14cd7abba4c06da1469
Aaron McDaid <aaronmcdaid@gmail.com>
2017-06-15 11:14:29 +0200

    from SR: added a few lines to 'test.wood.R' to read and print more stuff

--- tests/test.wood.R/test.wood.R
+++ tests/test.wood.R/test.wood.R
@@ -1,5 +1,6 @@
 rm(list = ls())
 
+Sys.setenv(LANG = "en")
 
 ## set paths
 ## -------------------
@@ -13,6 +14,7 @@ source("tests/r-function.R")
 
 ## run ssimp function
 ## ---------------
+## gwas: Allele1 is ref allele, and Allele2 alternative allele
 ssimp(path.gwas = "gwas/GIANT_HEIGHT_Wood_somechr22snps.txt.appendZcolumn.txt",
       path.ref  = "ref/1000genomes_somechr22snps_EURsamples.vcf",
       path.outdir = paste0(path.outdir , '/'),
@@ -22,8 +24,16 @@ ssimp(path.gwas = "gwas/GIANT_HEIGHT_Wood_somechr22snps.txt.appendZcolumn.txt",
       tag.snps = c("rs136382","rs136383", "rs136389"),
       #target.snps = "rs16987627", #it's biallelic, so we'll ignore it
       target.snps = c("rs5753220","rs5753231","rs5753236","rs5753259","rs5753260","rs5753263","rs5753268","rs5753271","rs5753272","rs5753281","rs5753284","rs5753285","rs5753290","rs5753298"),
-      impute.tags = FALSE
+      impute.tags = TRUE
       )
 
+## compare imputation of targets
+## -----------------------------
+out <- read_tsv(paste0(path.outdir,"/imputations.txt"))
+out
 
+## compare imputation of tags
+## -----------------------------
+out.tags <- read_tsv(paste0(path.outdir,"/imputations_tag.txt"))
+out.tags
 
