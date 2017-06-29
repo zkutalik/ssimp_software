4589557aea4ae83d5d815c3779ddccea59364839
Aaron McDaid <aaronmcdaid@gmail.com>
2017-06-29 16:37:49 +0200

    better liftover

--- liftover/liftover_dbsnp.R
+++ liftover/liftover_dbsnp.R
@@ -2,7 +2,7 @@
 ## Author: Sina Rueeger [sina *.* rueger *a*t* unil *.* ch]
 ## Project: 
 ##        
-## Time-stamp: <[liftover_dbsnp.R] by SR Thu 29/06/2017 15:57 (CEST)>
+## Time-stamp: <[liftover_dbsnp.R] by SR Thu 29/06/2017 16:37 (CEST)>
 ##
 ## Description:
 ## 
@@ -34,60 +34,17 @@ system(paste0(liftover," ",data.dbsnp,"liftover_hg19.bed /data/sgg/sina/software
 
 ## merge files together
 ## --------------------
-library(data.table)
-hg18 <- fread(paste0(data.dbsnp, "liftover_hg18.bed"),select = 3:5)
-hg19 <- fread(paste0(data.dbsnp, "liftover_hg19.bed"),select = 3:5)
-hg20 <- fread(paste0(data.dbsnp, "dbsnp_hg20.bed"),select = 3:5)
-names(hg18) <- c("Pos.hg18","rsid","Chr")
-names(hg19) <- c("Pos.hg19","rsid","Chr")
-names(hg20) <- c("Pos.hg20","rsid","Chr")
+cd /data/sgg/sina/public.data/dbsnp
+awk 'FNR==NR{a[$4]=$3;next}{if(a[$4]==""){a[$4]=0}; print $3,a[$4],$4,$5}' dbsnp_hg20.bed liftover_hg19.bed > tmp.txt
 
-setkey(hg18,rsid,Chr)
-setkey(hg19,rsid,Chr)
-setkey(hg20,rsid,Chr)
-comb = merge(hg18, merge(hg19, hg20, all = TRUE, sort = FALSE) , all = TRUE, sort = FALSE) 
+awk 'FNR==NR{a[$4]=$3;next}{if(a[$4]==""){a[$4]=0}; print a[$3],$1,$2,$3,$4}' liftover_hg18.bed tmp.txt > dbsnp_hg18_hg19_hg20.txt
+
+rm tmp.txt
 
 
+#FORMAT
+#dbsnp_hg18_hg19_hg20.txt
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## OLD
-
-tmp.18 <- (read_tsv(paste0(data.dbsnp,"output_hg18.bed"),col_names = FALSE))[,c(3:5)]
-tmp.19 <- (read_tsv(paste0(data.dbsnp,"output_hg19.bed"),col_names = FALSE))[,c(3:5)]
-tmp.20 <- (read_tsv(paste0(data.dbsnp,"toy_hg20.bed"),col_names = FALSE))[,c(3:5)]
-names(tmp.18) <- c("Pos.hg18","rsid","Chr")
-names(tmp.19) <- c("Pos.hg19","rsid","Chr")
-names(tmp.20) <- c("Pos.hg20","rsid","Chr")
-
-out <- merge(tmp.18, merge(tmp.19[,-3], tmp.20[,-3], by = "rsid"), by = "rsid")
-out <- out[,c("rsid", "Chr","Pos.hg18", "Pos.hg19", "Pos.hg20")]
-
-
-tmp <- read.table(paste0(data.dbsnp,"b150_SNPChrPosOnRef_108.bcp.gz"), nrow =10,
-                  header = FALSE, sep="\t")
-
-toyex <- data.frame(matrix(NA, ncol = 4, nrow = nrow(tmp)))
-toyex[,1] <- paste0("chr",tmp$V2) ## chr#
-toyex[,2] <- tmp$V3 ## pos-1
-toyex[,3] <- tmp$V3+1L ## pos
-toyex[,4] <- paste0("rs",tmp$V1) ## rs#
-toyex[,5] <- tmp$V2  ## CHR
-
-write_tsv(toyex, path = paste0(data.dbsnp, "toy_hg20.bed"), col_names = FALSE)
+#pos_hg18 pos_hg19 pos_hg20 rs  chr
+#91617493 91779557 92150243 rs7 7
+#...
