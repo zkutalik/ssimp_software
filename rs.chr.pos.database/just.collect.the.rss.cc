8ba340fd5fbe0f7fb0cad3e4d886e6717daa145f
Aaron McDaid <aaronmcdaid@gmail.com>
2017-08-12 07:15:15 +0000

    There are 81'237'339 rs-numbers in 1kg. That's a lot

--- rs.chr.pos.database/just.collect.the.rss.cc
+++ rs.chr.pos.database/just.collect.the.rss.cc
@@ -19,25 +19,20 @@ int main(int argc, char **argv) {
 
     ifstream  fs_1kg(argv[1]);
 
-    getline( fs_1kg, line);
-    fs_1kg || DIE("problem with 1kg");
-    line == "#CHROM\tPOS\tID" || DIE("problem with 1kg");
     while(getline( fs_1kg, line)) {
+        if(line == "#CHROM\tPOS\tID")
+            continue;
         //PP(line);
         auto tokens = utils:: tokenize(line, '\t');
         auto third = tokens.at(2);
 
         for(auto split_on_semicolons : utils:: tokenize(third, ';')) {
-            //PP(split_on_semicolons);
-            if(split_on_semicolons.substr(0, 3) == "esv")
-                continue;
-            if(split_on_semicolons.substr(0, 2) == "ss") 
-                continue;
-            split_on_semicolons.substr(0, 2) == "rs" || DIE("problem with 1kg");
-            split_on_semicolons = split_on_semicolons.substr(2);
-            auto rs_1kg = utils:: lexical_cast<int>(split_on_semicolons);
-            //PP(split_on_semicolons); PP(rs_1kg);
-            all_rs_in_any_file.insert(rs_1kg);
+            if(split_on_semicolons.substr(0, 2) == "rs") {
+                split_on_semicolons = split_on_semicolons.substr(2);
+                auto rs_1kg = utils:: lexical_cast<int>(split_on_semicolons);
+                //PP(split_on_semicolons); PP(rs_1kg);
+                all_rs_in_any_file.insert(rs_1kg);
+            }
         }
 
     }
