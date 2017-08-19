4458593039ef202f4ff9992b41096e90000b654a
Aaron McDaid <aaronmcdaid@gmail.com>
2017-08-19 13:05:53 +0000

    Wrap it in a try.catch, and other little tweaks

--- rs.chr.pos.database/build.the.binary.cc
+++ rs.chr.pos.database/build.the.binary.cc
@@ -76,35 +76,44 @@ int main(int argc, char **argv) {
             if(chrom_string             == "MT")
                 continue;
             assert(split.at(3).substr(0,2) == "rs");
-            int chrom = [&]() ->int {
-                if(chrom_string=="X") return static_cast<int>( three_builds_t:: XYZ:: CHROM_X );
-                if(chrom_string=="Y") return static_cast<int>( three_builds_t:: XYZ:: CHROM_Y );
-                return utils:: lexical_cast<int>(chrom_string);
-            }();
-            int posa  = utils:: lexical_cast<int>(split.at(1));
-            int posb  = utils:: lexical_cast<int>(split.at(2));
-            int rs    = utils:: lexical_cast<int>(split.at(3).substr(2));
-            //PP(chrom, posa, rs);
-            assert(posb == posa+1);
 
-            auto entry = three_builds_for_all_interesting_snps.find(rs);
-            if(entry != three_builds_for_all_interesting_snps.end()) {
-                if(entry->second.chrom == -1) {
-                    entry->second.chrom = chrom;
-                }
-                assert(entry->second.chrom == chrom);
+            try {
+                int chrom = [&]() ->int {
+                    if(chrom_string=="X") return static_cast<int>( three_builds_t:: XYZ:: CHROM_X );
+                    if(chrom_string=="Y") return static_cast<int>( three_builds_t:: XYZ:: CHROM_Y );
+                    return utils:: lexical_cast<int>(chrom_string);
+                }();
+                int posa  = utils:: lexical_cast<int>(split.at(1));
+                int posb  = utils:: lexical_cast<int>(split.at(2));
+                int rs    = utils:: lexical_cast<int>(split.at(3).substr(2));
+                //PP(chrom, posa, rs);
+                assert(posb == posa+1);
 
-                int * position_under_current_build = nullptr;
-                switch(argv_index) {
-                    break; case 1: position_under_current_build = & entry->second.hg18;
-                    break; case 2: position_under_current_build = & entry->second.hg19;
-                    break; case 3: position_under_current_build = & entry->second.hg20;
-                    break; default: DIE("argv_index out of range");
+                auto entry = three_builds_for_all_interesting_snps.find(rs);
+                if(entry != three_builds_for_all_interesting_snps.end()) {
+                    if(entry->second.chrom == -1) {
+                        entry->second.chrom = chrom;
+                    }
+                    if(entry->second.chrom != chrom) {
+                        PP(line, split, chrom, entry->second.chrom);
+                    }
+                    assert(entry->second.chrom == chrom);
+
+                    int * position_under_current_build = nullptr;
+                    switch(argv_index) {
+                        break; case 1: position_under_current_build = & entry->second.hg18;
+                        break; case 2: position_under_current_build = & entry->second.hg19;
+                        break; case 3: position_under_current_build = & entry->second.hg20;
+                        break; default: DIE("argv_index out of range");
+                    }
+                    assert(position_under_current_build);
+                    assert(*position_under_current_build  == -1);
+                    *position_under_current_build  = posa; // zero-based, I think - https://genome.ucsc.edu/FAQ/FAQformat.html#format1
+                    assert(*position_under_current_build  != -1);
                 }
-                assert(position_under_current_build);
-                assert(*position_under_current_build  == -1);
-                *position_under_current_build  = posa; // zero-based, I think - https://genome.ucsc.edu/FAQ/FAQformat.html#format1
-                assert(*position_under_current_build  != -1);
+            } catch (std:: invalid_argument &) {
+                PP(line, split);
+                exit(1);
             }
         }
         fs_one_build.eof() || DIE("fs_one_build.eof()");
