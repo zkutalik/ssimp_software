ce0f8e1d0a1252699f8e167414a8747476e2b602
Sina Rüeger <sinarueeger@users.noreply.github.com>
2017-08-29 12:25:28 +0200

    Update transform_z_to_b.R

--- transform_z_to_b.R
+++ transform_z_to_b.R
@@ -1,23 +1,24 @@
+## Transform the imputed Z-statistics to imputed effect sizes
+## --------------------------------------------------------------
+## Assumptions: - if your gwas Z-stats originates from a linear regression with a standardised outcome (variance = 1)
+##              - if the effect sizes are small 
 
-
-head(output)
-
-
-## adds b_imp and se_imp
-## linear regression!
+## this function calculates b_imp and se_imp
 f.z2b <- function(z, af, n)
 {
   
-  se <- 1/sqrt(2* af * (1-af)* n)
+    se.b <- 1/sqrt(2* af * (1-af)* n)
   
-    b <- z * se
+    b <- z * se.b
     
     return(c(b, se))
   
   
 }
 
-
+## example
+## ---------
+output <- read.table()
 N.max <- 1000
 output$N_imp <- output$r2.pred * N.max
 
