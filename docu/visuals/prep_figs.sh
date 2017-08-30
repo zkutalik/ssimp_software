e84165544cfcb6a2e986b8a646bf98cdfef7d5f7
Sina Rueeger <sina.rueeger@gmail.com>
2017-08-30 17:41:59 +0200

    blabla

--- docu/visuals/prep_figs.sh
+++ docu/visuals/prep_figs.sh
@@ -2,5 +2,5 @@
 for file in *.pdf; do \
 echo $file;\
 convert -trim -density 900 -quality 100 $file `echo $file|cut -f1 -d'.'`.jpg;\
-convert `echo $file|cut -f1 -d'.'`.jpg -resize 30% `echo $file|cut -f1 -d'.'`.jpg;\
+convert `echo $file|cut -f1 -d'.'`.jpg -resize 10% `echo $file|cut -f1 -d'.'`.jpg;\
 done
